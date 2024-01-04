9a4ca4932f5254785ebf424611407ada5190c59e
Mehul Pathak <pathakkmehul@gmail.com>
2024-01-04 22:00:10 +0530

    first commit

---
+++ js/deleteItems.js
@@ -0,0 +1,34 @@
+$(function() {
+  $("#remove").click(function() {
+    var confirm = window.confirm("Are you sure you want to delete this item?");
+    if (confirm == true) {
+      $("#item").hide();
+      alert("Item deleted!");
+      
+    } else {
+      alert("Item not deleted");
+    }
+  });
+  $("#remove2").click(function() {
+    var confirm = window.confirm("Are you sure you want to delete this item?");
+    if (confirm == true) {
+      $("#item1").hide();
+      alert("Item deleted!");
+    } else {
+      alert("Item not deleted");
+    }
+  });
+  $("#remove3").click(function() {
+    var confirm = window.confirm("Are you sure you want to delete this item?");
+    if (confirm == true) {
+      $("#item2").hide();
+      alert("Item deleted!");
+    } else {
+      alert("Item not deleted");
+    }
+    $("hr").hide();
+  });
+  
+
+  
+});
